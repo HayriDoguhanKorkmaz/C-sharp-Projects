88231cc7099dd391c2ad41873d7290093d3ac34e
Hayri Doğuhan Korkmaz <90601272+HayriDoguhanKorkmaz@users.noreply.github.com>
2022-05-08 13:17:28 +0300

    Add files via upload

---
+++ iddiaUygulaması/iddiaUygulaması/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace iddiaUygulaması
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ev sahibi takım       ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(135, 30);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(122, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Deplasman takım\r\n";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(135, 73);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(122, 27);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(264, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 73);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Tahmin Et!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 19;
+            this.listBox1.Location = new System.Drawing.Point(12, 129);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(414, 80);
+            this.listBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(348, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 73);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Temizle!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(432, 21);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(267, 188);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(356, 19);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Gerçek kurum ya da kuruluşlarla hiçbir alakası yoktur";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(704, 248);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "İddia Programı";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ iddiaUygulaması/iddiaUygulaması/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iddiaUygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string evSahibi = textBox1.Text;
+            string deplasman = textBox2.Text;
+
+            Random random = new Random();
+            int olasilik = random.Next(0, 101);
+
+
+            if (olasilik <= 10)
+            {
+                listBox1.Items.Add(evSahibi + " - " + deplasman + " maçını kazanan " + evSahibi);
+            }
+            else if (olasilik <= 20)
+            {
+                listBox1.Items.Add(evSahibi + " - " + deplasman + " maçını kazanan " + deplasman);
+            }
+            else if (olasilik <= 30)
+            {
+                listBox1.Items.Add(evSahibi + " - " + deplasman + " maç sonucu 1.5 üst!");
+            }
+            else if (olasilik <= 40)
+            {
+                listBox1.Items.Add(evSahibi + " - " + deplasman + " maç sonucu 2.5 üst!");
+            }
+            else if (olasilik <= 50)
+            {
+                listBox1.Items.Add(evSahibi + " - " + deplasman + " maç sonucu 3.5 üst!");
+            }
+            else if (olasilik <= 60)
+            {
+                listBox1.Items.Add(evSahibi + " - " + deplasman + " karşılıklı gol var!");
+            }
+            else if (olasilik <= 70)
+            {
+                listBox1.Items.Add(evSahibi + " - " + deplasman + " karşılıklı gol yok!");
+            }
+            else if (olasilik <= 80)
+            {
+                listBox1.Items.Add(evSahibi + " - " + deplasman + " ilk yarı 0.5 üst!");
+            }
+            else if (olasilik <= 90)
+            {
+                listBox1.Items.Add(evSahibi + " - " + deplasman + " ilk yarı 1.5 üst!");
+            }
+            else if (olasilik <= 100)
+            {
+                listBox1.Items.Add(evSahibi + " - " + deplasman + " maçının sonucu beraberlik!");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+    }
+}
